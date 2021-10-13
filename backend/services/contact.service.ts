dcb5a5cef9eaf6f9a6e455e454a34f565c71b310
Pascal Garber <pascal@artandcode.studio>
2021-10-13 12:01:47 +0200

    Get social urls but need to wait for bugfix

--- backend/services/contact.service.ts
+++ backend/services/contact.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from "alosaur/mod.ts";
 import { vCard } from "vcard/mod.ts";
 import { StrapiService } from "./strapi.service.ts";
+import { SocialLinkService } from "./sozial-link.service.ts";
 import { StrapiRestAPIContact } from "../types/strapi-rest-api-contact.ts";
 import { StrapiImage } from "../types/strapi-image.ts";
+import { StrapiRestAPISocialLink } from "../types/strapi-rest-api-social-link.ts";
 
 @Injectable()
 export class ContactService {
-  private strapi = new StrapiService("contact");
+  private readonly strapi = new StrapiService("contact");
 
-  constructor() {}
+  constructor(private readonly social: SocialLinkService) {}
 
   public async get() {
     try {
@@ -39,6 +41,9 @@ export class ContactService {
       countryRegion: contact.countryRegion,
     };
 
+    // Wait for fix: https://github.com/timpeq/vCards-ts/issues/1
+    // vcard.socialUrls = await this.getSocialUrls();
+
     vcard.photo = {
       url: this.strapi.getRemoteStrapiImageUrl(contact.photo),
       mediaType: this.getImageType(contact.photo),
@@ -51,5 +56,19 @@ export class ContactService {
   public getImageType(image: StrapiImage) {
     const type = image.mime.replace("image/", "");
     return type;
+  }
+
+  public async getSocialUrls(socialLinks?: StrapiRestAPISocialLink[]) {
+    socialLinks = socialLinks || await this.social.list();
+    const socialUrls = new Map<string, string>();
+    for (const socialLink of socialLinks) {
+      const platform = socialLink.icon.replaceAll("social_", "").replaceAll(
+        "_circle",
+        "",
+      ).replaceAll("_square", "").replaceAll(".svg", "");
+      socialUrls.set(platform, socialLink.url);
+    }
+
+    return socialUrls;
   }
 }
