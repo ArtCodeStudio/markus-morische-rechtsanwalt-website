27fa752c1ee9cb609d999b350dbaabaeea641dc3
Pascal Garber <pascal@artandcode.studio>
2022-02-16 20:53:49 +0100

    Add pm2 dev script for backend

--- backend/pm2.dev.config.js
+++ backend/pm2.dev.config.js
@@ -3,10 +3,11 @@ module.exports = {
       {
         name: "dev:@markus-morische/backend",
         script: "deno run --allow-run scripts.ts watch",
-	      // args: "./app.ts",
-        // interpreter: "deno",
-	      // interpreter_args: "run --allow-net --allow-read --importmap=imports.json --allow-env --config ./tsconfig.json",
-        // env: {},
+        env: {
+          SERVER_PORT: "4001",
+          STRAPI_REMOTE_URL: "http://localhost:4002/api",
+          STRAPI_LOCAL_URL: "http://localhost:4002/api",
+        },
       },
     ],
   };
