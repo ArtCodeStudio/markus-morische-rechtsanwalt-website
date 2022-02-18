4e07e9d2359110a289b83592a4ad51ea97a84e22
Pascal Garber <pascal@artandcode.studio>
2022-02-18 17:39:44 +0100

    Change requests

--- backend/services/cache.service.ts
+++ backend/services/cache.service.ts
@@ -4,8 +4,12 @@ import { Cache, IndexSignatureKey } from "local_cache/mod.ts";
 @Singleton()
 export class CacheService<K extends IndexSignatureKey, V> extends Cache<K, V> {
     constructor() {
-        // global ttl of 30 seconds
-        super(-1); // Endless ttl
+        let ttl = -1; // Endless ttl
+        if (Deno.env.get("ENV") === 'local') {
+            // ttl of 3 seconds
+            ttl = 3000
+        }
+        super(ttl);
         console.debug("Create new cache service");
     }
 }
